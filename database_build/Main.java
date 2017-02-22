2224a245beba0a70d19a1435223a66bf8b866dff
David <ack.dav@gmail.com>
2017-02-22 11:41:01 +0100

    added plotting and accuracy to neural net

---
+++ database_build/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.apache.jena.sparql.algebra.*;
+import org.apache.jena.query.*;
+import org.apache.jena.*;
+
+public class Main {
+
+	static String appendPrefixes(String query) {
+		String prefix = new String();
+
+		try (BufferedReader br = new BufferedReader(new FileReader("jena-missing-prefixes.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				prefix += line;
+			}
+			prefix += ' ';
+		}
+		catch (Exception e){
+			System.out.println("Error: " + e);
+		}
+
+		// query = prefix + query;
+		// System.out.println(query);
+		return query;
+	}
+
+	public static void main(String args[]){
+		String noBackslash = args[0].replaceAll("\\\\", "");
+
+		String appendedQuery = appendPrefixes(noBackslash);
+
+		// parser.parse(QueryParser.escape(appendedQuery))
+		Query query = QueryFactory.create(appendedQuery);
+		Op op = Algebra.compile(query) ;
+		System.out.println(op);
+	}
+}
---
+++ database_build/build_db.py
@@ -0,0 +1,70 @@
+from __future__ import absolute_import
+
+import re, sys, requests, time
+from urlparse import urlparse, parse_qs
+import urllib
+
+from sparql_graph.query_vector_converter import convert_query
+
+fallback_json = { "head": { "link": [], "vars": ["property", "propertyLabel", "propertyVal", "propertyValLabel"] },
+  "results": { "distinct": False, "ordered": True, "bindings": [ ] } }
+
+def run_http_request(req):
+	url = 'http://claudio11.ifi.uzh.ch:8890' + req + '&format=json'
+	t0 = time.clock()
+	# make call and measure time taken
+	resp = requests.get(url)
+	time1 = time.clock() - t0
+
+	return resp, time1
+
+def write_db(query, time, result_size):
+	with open("tf-db-cold.txt", 'a') as f:
+		# Remove formatting from log to stay consistent (not every url is formatted)
+		line_no_tabs = re.sub(r'%09|%0B', '+', query)
+		line_single_spaces = re.sub(r'\++', '+', line_no_tabs)
+		line_no_formatting = re.sub(r'%0A|%0D', '', line_single_spaces)
+		line_noprefix = re.sub(r'.*query=', '', line_no_formatting)
+		line_noquotes = re.sub(r'"', '', line_noprefix)
+		line_end_format = re.sub(r'(&.*?)$', '', line_noquotes)
+
+		#decode url nicely into SPARQL query
+		decoded = urllib.unquote_plus(line_end_format.encode('ascii'))
+
+		query_vec = convert_query(decoded)
+		if query_vec != -1:
+			query_vec.insert(len(query_vec), time)
+			# write db
+			f.write(decoded + '\t' + str(query_vec) + '\t' + str(result_size) + '\n')
+			print 'wrote query '
+
+def run_log(log_file):
+	# open queries and regex for links
+	urls = re.findall('"GET (.*?) HTTP', open (log_file).read())
+
+	for index, line in enumerate(urls):
+		resp, time = run_http_request(line)
+
+		try:
+			respJson = resp.json()
+		except:
+			respJson = fallback_json
+
+		#try to fetch result size
+		try:
+			result_size = len(respJson['results']['bindings'])
+		except:
+			result_size = 0
+		
+		if result_size > 0:
+			write_db(line, time, result_size)
+
+		else:
+			print '.',
+
+def main():
+	run_log('log20k.log')
+
+
+if __name__ == '__main__':
+	main()
