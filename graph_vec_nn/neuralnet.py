29d0972e94714b294c78698a6e0355b064ea79ca
David <ack.dav@gmail.com>
2017-02-23 19:02:51 +0100

    corrected accuracy and plotting

--- graph_vec_nn/neuralnet.py
+++ graph_vec_nn/neuralnet.py
@@ -17,11 +17,12 @@ x_vals_test = np.array([])
 y_vals_test = np.array([])
 num_training_samples = 0
 batch_size = 10
-training_epochs = 200
+training_epochs = 300
 
 # Training loop
 loss_vec = []
 test_loss = []
+avg_cost_vec = []
 
 # Normalize by column (min-max norm to be between 0 and 1)
 def normalize_cols(m):
@@ -87,6 +88,11 @@ def neural_net_model(data):
 
 	return output
 
+
+def accuracy(predictions, labels):
+    return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+            / predictions.shape[0])
+
 def train_neural_network(x):
 	prediction = neural_net_model(x)
 	sig = tf.sigmoid(prediction)
@@ -98,8 +104,9 @@ def train_neural_network(x):
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		for epoch in range(training_epochs):
-			temp_loss = 0
+			temp_loss = 0.
 			avg_cost = 0.
+
 			total_batch = int(round(num_training_samples/batch_size))
 			for i in range(total_batch-1):
 				batch_x = x_vals_train[i*batch_size:(i+1)*batch_size]
@@ -107,36 +114,43 @@ def train_neural_network(x):
 				# Run optimization op (backprop) and cost op (to get loss value)
 				_, c, p = sess.run([optimizer, cost, prediction], feed_dict={x: batch_x,
     			                                          y: np.transpose([batch_y])})
-				loss_vec.append(temp_loss)
+				loss_vec.append(c)
 				# Compute average loss
 				avg_cost += c / total_batch
+				avg_cost_vec.append(avg_cost)
+
 
 			# sample prediction
 	 		label_value = batch_y
 	 		estimate = p
 	 		err = label_value-estimate
 	 		# print ("num batch:", i)
-
+	 		perc_err = tf.reduce_mean((batch_y-p)/batch_y)
 			# Display logs per epoch step
 			if epoch % 50 == 0:
 				print ("Epoch:", '%04d' % (epoch+1), "cost=", \
 					"{:.9f}".format(avg_cost))
 				print ("[*]----------------------------")
-				for i in xrange(3):
+				for i in xrange(4):
 					print ("label value:", label_value[i], \
 						"estimated value:", estimate[i])
 				print ("[*]============================")
 
-		correct_prediction = tf.equal(tf.round(tf.clip_by_value(prediction,0,1)), y)
-		correct_prediction = tf.Print(correct_prediction, [correct_prediction])
-		accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+		perc_err = tf.reduce_mean((y-prediction)/y)
+		
+		correct_prediction = tf.less(tf.cast(perc_err, "float"), 4.7)
 
-		print('Accuracy:', accuracy.eval({x: x_vals_test, y: np.transpose([y_vals_test])}))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
 
-def plot_result(loss_vec, test_loss):
+		print "Test accuracy: {:.3f}".format(accuracy.eval({x: x_vals_test, y: np.transpose([y_vals_test])}))
+		plot_result(loss_vec, avg_cost_vec)
+
+def plot_result(loss_vec, avg_cost_vec):
 	# Plot loss (MSE) over time
 	plt.plot(loss_vec, 'k-', label='Train Loss')
-	plt.plot(test_loss, 'r--', label='Test Loss')
+	plt.plot(avg_cost_vec, 'r--', label='Test Loss')
+	# plt.plot(test_loss, 'b--', label='root squared mean error')
+
 	plt.title('Loss (MSE) per Generation')
 	plt.xlabel('Generation')
 	plt.ylabel('Loss')
