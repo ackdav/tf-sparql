35a2ed4c2446c0af5df48590b158a30c80439e58
derdav3 <ack.dav@gmail.com>
2017-03-15 15:28:10 +0100

    added tensorboard implementation

---
+++ database_build/sparql_graph/query_vector_converter.py
@@ -0,0 +1,152 @@
+'''
+Executes Main.java in the same directory and prints it out
+'''
+import subprocess, re, sys, collections, os.path
+from subprocess import STDOUT,PIPE,Popen
+import pyparsing as pp
+from collections import defaultdict
+
+# def compile_java(java_file):
+# 	cmd = ["javac", "-classpath", '/Users/David/libs/jena/lib/*:.', java_file]
+# 	proc = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
+# 	stdout, stderr = proc.communicate()
+# executes Main.java in same folder to convert to SPARQL Algebra expressino
+
+# def clean_query(query):
+# 	query = re.sub(r'([A-Z]{3,})', r' \1', query)
+# 	query = ' '.join(query.split())
+# 	return query
+
+def structural_query_vector(query):
+	result = -1
+
+	try:
+		result = jena_graph('Main', query)
+		result = algebra_structure_feature_vector(result[0])
+	except:
+		print "algebra_structure_feature_vector err", sys.exc_info()[0]
+		return -1
+	return result
+
+def jena_graph(java_file, args):
+	'''
+	Starts Main.java in the same folder and converts it into a query tree, then into a nested list
+	'''
+	graph = ''
+	#Makes the call to start Main.java - gets output of file via System.out.println(string)
+	cmd = ["java", "-cp", "/Users/david/libs/jena/lib/*:.", java_file, args]
+	proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+	stdout, stderr = proc.communicate()
+
+	for line in stdout:
+		graph += line
+
+	try:
+		res_graph = pp.nestedExpr(opener='(', closer=')').parseString(graph)
+		res_graph = res_graph.asList()
+	except:
+		print "pyparse err", graph, args
+		res_graph = -1
+	return res_graph
+
+
+def bgp_type_match(bgp_list):
+	'''Matching to one of 8 BGP types:
+	1. ?s o p 	5. ?s o ?p
+	2. s ?o p 	6. s ?o ?p
+	3. s o ?p 	7. ?s ?o ?p
+	4. ?s ?o p 	8. s o p
+	'''
+
+	if bgp_list[1][0] == '?':
+		if bgp_list[2][0] == '?':
+			if bgp_list[3][0] == '?':
+				return ('bgptype', 7)
+			else:
+				return ('bgptype', 4)
+		elif bgp_list[3][0] == '?':
+			return ('bgptype', 5)
+		else:
+			return ('bgptype', 1)
+	elif bgp_list[2][0] == '?':
+		if bgp_list[3][0] == '?':
+			return ('bgptype', 6)
+		else:
+			return ('bgptype', 2)
+	elif bgp_list[3][0] == '?':
+		return ('bgptype', 3)
+	else:
+		return ('bgptype', 8)
+
+def tokenize_list(nested_list, d=0):
+	if not isinstance(nested_list, list):
+		yield nested_list, d
+	else:
+		if nested_list[0] == 'triple':
+			# !!! second value of bgptype represents type, and not depth
+			yield  bgp_type_match(nested_list)
+		for item in nested_list:
+			for x in tokenize_list(item, d=d+1):
+				# if x[0][0]!='?' and x[0][0]!='<' and x[0]!='triple':
+				# if x[0][0]!='?' and x[0][0]!='<' and x[0][0]!='>':
+				yield x
+	return
+
+def sparql_structure_feature_vector(query):
+	print "hi"
+
+def algebra_structure_feature_vector(tree):
+	'''
+	query_cmds def per index:
+	1. number of occurences
+	2. min height in graph
+	3. max height in graph
+	'''
+	maxdepth = 0
+	query_cmds = {'maxdepth': [0], 'wordcount': [0],\
+					 'triple': [0,0,0], 'bgp':[0,0,0], 'leftjoin':[0,0,0], 'union':[0,0,0], \
+						'tolist': [0,0,0], 'order': [0,0,0], 'project':[0,0,0], 'distinct': [0,0,0],\
+						'reduced': [0,0,0], 'multi': [0,0,0], 'top':[0,0,0], 'group': [0,0,0], \
+							'assign': [0,0,0], 'sequence':[0,0,0], 'bgptype':[0,0,0,0,0,0,0,0] }
+
+	tokenized_graph_list = list(tokenize_list(tree))
+	query_cmds['wordcount'][0] = len(tokenized_graph_list)
+	# print "LENGTH", len(tokenized_graph_list)
+	# TEST: test structural feature effectiveness
+	# if len(tokenized_graph_list) <= 8:
+	# 	return -1
+	for val, depth in tokenized_graph_list:
+		if depth > maxdepth:
+			maxdepth = depth
+		if val.lower() in query_cmds:
+			#handle bgptypes - depth == bgptype here and not depth
+			if val.lower() == 'bgptype':
+				query_cmds[val.lower()][depth-1] += 1
+			else:
+				#handle rest
+				query_cmds[val.lower()][0] += 1
+				#if new entry - initialize min and max depth
+				if query_cmds[val.lower()][0] == 1:
+					query_cmds[val.lower()][1] = 1
+					query_cmds[val.lower()][2] = 1
+				else:
+					if query_cmds[val.lower()][1] > depth:
+						query_cmds[val.lower()][1] = depth
+					if query_cmds[val.lower()][2] < depth:
+						query_cmds[val.lower()][2] = depth
+	query_list = []
+	for key in sorted(query_cmds):
+		query_list = query_list + query_cmds[key]
+	return query_list
+
+
+
+def main():
+	# compile_java('Main.java')
+	# escaped = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>SELECT DISTINCT ?anton_tchekov ?anton_tchekov_field_auteurWHERE {?anton_tchekov rdfs:label "Anton Tchekov"@en; rdfs:label ?anton_tchekov_field_auteur. } LIMIT 5".replace('"','\\"')
+	# print escaped
+	print structural_query_vector("SELECT*{{<http://dbpedia.org/resource/Tomb_Raider:_The_Angel_of_Darkness> ?po ?x} UNION{?x ?pi <http://dbpedia.org/resource/Tomb_Raider:_The_Angel_of_Darkness>}} ORDER BY ?pi ?po ?x")
+
+
+if __name__ == '__main__':
+	main()
--- graph_vec_nn/neuralnet2.py
+++ graph_vec_nn/neuralnet2.py
@@ -3,17 +3,21 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+tf.reset_default_graph()
+
 from tensorflow.contrib import learn
 import matplotlib.pyplot as plt
-import re, ast
+import re, ast, os
 from random import sample
 from sklearn.pipeline import Pipeline
 from sklearn import datasets, linear_model
 from sklearn import cross_validation
 import numpy as np
 
+LOGDIR = '/tmp/'
+
 # Parameters
-learning_rate = 0.01
+learning_rate = 0.005
 training_epochs = 600
 batch_size = 150
 display_step = 1
@@ -28,9 +32,11 @@ X_train = np.array([])
 Y_train = np.array([])
 X_test = np.array([])
 Y_test = np.array([])
+y_vals = np.array([])
+no_model_error = 0.
 # tf Graph input
-x = tf.placeholder("float", [None, 78])
-y = tf.placeholder("float", [None,1])
+x = tf.placeholder("float", [None, 78], name="x")
+y = tf.placeholder("float", [None,1], name="y")
 
 def normalize_cols(m):
     col_max = m.max(axis=0)
@@ -43,6 +49,7 @@ def load_data():
     global X_test
     global Y_train
     global X_train
+    global y_vals
     global num_training_samples
 
     volume = 0.
@@ -81,58 +88,77 @@ def load_data():
     X_train = np.nan_to_num(normalize_cols(X_train))
     X_test = np.nan_to_num(normalize_cols(X_test))
 
+def no_modell_mean_error(y_vals):
+    mean_ = y_vals.mean()
+    mean_sum = 0.
+    for y_ in y_vals:
+        mean_error = abs(y_- mean_) / mean_
+        mean_sum += mean_error
+    return mean_sum / y_vals.shape[0]
+
 n_input = X_train.shape[0]
 load_data()
 # Create model
-def multilayer_perceptron(x, weights, biases):
-    # Hidden layer with RELU activation
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
 
-    # Hidden layer with RELU activation
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
+def multilayer_perceptron(x, weights, biases, name="neuralnet"):
+    with tf.name_scope(name):
+        # Hidden layer with RELU activation
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        layer_1 = tf.nn.relu(layer_1)
 
-    # Hidden layer with RELU activation
-    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
-    layer_3 = tf.nn.relu(layer_3)
+        # Hidden layer with RELU activation
+        layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        layer_2 = tf.nn.relu(layer_2)
 
-    # Hidden layer with RELU activation
-    layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
-    layer_4 = tf.nn.relu(layer_4)
+        # Hidden layer with RELU activation
+        layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
+        layer_3 = tf.nn.relu(layer_3)
 
-    # Output layer with linear activation
-    out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
-    return out_layer
+        # Hidden layer with RELU activation
+        layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
+        layer_4 = tf.nn.relu(layer_4)
+
+        # Output layer with linear activation
+        out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
+        return out_layer
 
 # Store layers weight & bias
 weights = {
-    'h1': tf.Variable(tf.random_normal([78, n_hidden_1], 0, 0.1)),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], 0, 0.1)),
-    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3], 0, 0.1)),
-    'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4], 0, 0.1)),
-    'out': tf.Variable(tf.random_normal([n_hidden_4, 1], 0, 0.1))
+    'h1': tf.Variable(tf.random_normal([78, n_hidden_1], 0, 0.1), name="h1W"),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], 0, 0.1), name="h2W"),
+    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3], 0, 0.1), name="h3W"),
+    'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4], 0, 0.1), name="h4W"),
+    'out': tf.Variable(tf.random_normal([n_hidden_4, 1], 0, 0.1), name="hOutW")
 }
 biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1], 0, 0.1)),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2], 0, 0.1)),
-    'b3': tf.Variable(tf.random_normal([n_hidden_3], 0, 0.1)),
-    'b4': tf.Variable(tf.random_normal([n_hidden_4], 0, 0.1)),
-    'out': tf.Variable(tf.random_normal([1], 0, 0.1))
+    'b1': tf.Variable(tf.random_normal([n_hidden_1], 0, 0.1), name="h1W"),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2], 0, 0.1), name="h2W"),
+    'b3': tf.Variable(tf.random_normal([n_hidden_3], 0, 0.1), name="h3W"),
+    'b4': tf.Variable(tf.random_normal([n_hidden_4], 0, 0.1), name="h4W"),
+    'out': tf.Variable(tf.random_normal([1], 0, 0.1), name="hOutW")
 }
 
 # Construct model
 prediction = multilayer_perceptron(x, weights, biases)
 # pred = np.transpose([pred])
 # Define loss and optimizer
-cost = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y, prediction))))
+with tf.name_scope("RMSE"):
+    cost = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y, prediction))), name="RMSE")
+    tf.summary.scalar("RMSE", cost)
 
-optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate).minimize(cost)
+with tf.name_scope("train"):
+    optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate).minimize(cost)
+
+summ = tf.summary.merge_all()
 
 # Launch the graph
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
 
+    sess.run(tf.global_variables_initializer())
+    #TensorBoard 
+    writer = tf.summary.FileWriter(LOGDIR)
+    writer.add_graph(sess.graph)
     # Training cycle
     for epoch in range(training_epochs):
         avg_cost = 0.
@@ -147,13 +173,23 @@ with tf.Session() as sess:
             # Compute average loss
             avg_cost += c / total_batch
 
+            if i % 10 == 0:
+                [train_accuracy, s] = sess.run([cost, summ], feed_dict={x: batch_x, y: batch_y})
+                writer.add_summary(s, i)
+
+
         # sample prediction
         label_value = batch_y
         estimate = p
         err = label_value-estimate
 
         # Display logs per epoch step
-        if epoch % 10 == 0:
+        if epoch % 50 == 0:
+            # sess.run(assignment, feed_dict={x: X_test, y: Y_test})
+            saver.save(sess, os.path.join(LOGDIR, "model.ckpt"), epoch)
+            tf.summary.scalar("perc_error", perc_error)
+
+
             print ("Epoch:", '%04d' % (epoch+1), "cost=", \
                 "{:.9f}".format(avg_cost))
             print ("[*]----------------------------")
@@ -163,10 +199,19 @@ with tf.Session() as sess:
             print ("[*]============================")
 
     print ("Optimization Finished!")
-    perc_err = tf.reduce_mean(tf.divide(tf.abs(tf.subtract(y, prediction)), tf.reduce_mean(y)))
-    rmse = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y, prediction))))
+    with tf.name_scope("relativeMeanError"):
+        perc_err = tf.reduce_mean(tf.divide(tf.abs(tf.subtract(y, prediction)), tf.reduce_mean(y)))
+        rmse = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y, prediction))))
 
-    # mean_relative_error = tf.divide(tf.to_float(tf.reduce_sum(perc_err)), Y_test.shape[0])
-    Y_test = np.transpose([Y_test])
-    print ("RMSE: {:.3f}".format(rmse.eval({x: X_test, y: Y_test})))
-    print ("relative error: {:.3f}".format(perc_err.eval({x: X_test, y: Y_test})))
+        # mean_relative_error = tf.divide(tf.to_float(tf.reduce_sum(perc_err)), Y_test.shape[0])
+        Y_test = np.transpose([Y_test])
+        print ("RMSE: {:.3f}".format(rmse.eval({x: X_test, y: Y_test})))
+        print ("relative error with model: {:.3f}".format(perc_err.eval({x: X_test, y: Y_test})), "without model: {:.3f}".format(no_modell_mean_error(y_vals)))
+
+        tf.summary.scalar("relative_mean_error", perc_err)
+
+# def main():
+
+
+# if __name__ == '__main__':
+#     main()
