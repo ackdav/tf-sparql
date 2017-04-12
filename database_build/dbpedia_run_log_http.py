8eb8ac615665e5fd58ec152aedbd37ed046eb440
David <ack.dav@gmail.com>
2017-04-12 21:47:56 +0200

    update readme

--- database_build/dbpedia_run_log_http.py
+++ database_build/dbpedia_run_log_http.py
@@ -9,7 +9,12 @@ fallback_json = { "head": { "link": [], "vars": ["property", "propertyLabel", "p
   "results": { "distinct": False, "ordered": True, "bindings": [ ] } }
 
 def run_http_request(req):
-	url = 'http://claudio11.ifi.uzh.ch:8890' + req + '&format=json'
+	'''Executes HTTP request to server and returns time
+
+	Keyword-args:
+	req -- sparql query in url formatting
+	'''
+	url = 'http://claudio11.ifi.uzh.ch:8890' + req + '&timeout=20000&format=json'
 	t0 = time.clock()
 	# make call and measure time taken
 	resp = requests.get(url)
@@ -17,6 +22,11 @@ def run_http_request(req):
 	return resp, time1
 
 def cleanup_query(query):
+	'''Cleans log-url into readable sparql query
+
+	Keyword-args:
+	query -- log-url to clean
+	'''
 	line_no_tabs = re.sub(r'%09|%0B', '+', query)
 	line_single_spaces = re.sub(r'\++', '+', line_no_tabs)
 	line_no_formatting = re.sub(r'%0A|%0D', '', line_single_spaces)
@@ -24,7 +34,6 @@ def cleanup_query(query):
 	line_noquotes = re.sub(r'"', '', line_noprefix)
 	line_end_format = re.sub(r'(&.*?)$', '', line_noquotes)
 
-	#decode url nicely into SPARQL query
 	return urllib.unquote_plus(line_end_format.encode('ascii'))
 
 def get_result_size(response):
@@ -35,23 +44,22 @@ def get_result_size(response):
 		result_size = 0
 	return result_size
 
-
 def run_log(query_line):
 	# open queries and regex for links
 	url_ = re.findall('"GET (.*?) HTTP', query_line)
 
 	if len(url_) == 1:
 		request_url = url_[0]
-
 		query_times = []
 		resp = ''
 		result_size = 0
 		
 		try:
 			# use if warm execution times are needed
-			# for _ in range(11):
-			response, exec_time = run_http_request(request_url)
-			query_times.append(exec_time)
+			for _ in range(11):
+				response, exec_time = run_http_request(request_url)
+				query_times.append(exec_time)
+				# time.sleep(random.random()*2)
 			
 			respJson = response.json()
 			result_size = get_result_size(respJson)
@@ -65,13 +73,13 @@ def run_log(query_line):
 
 		if exec_time != -1: #and result_size > 0:
 			cold_exec_time = query_times[0]
-			# warm_times = query_times[1:]
-			# warm_mean = np.mean(warm_times, dtype=np.float64)
+			warm_times = query_times[1:]
+			warm_mean = np.mean(warm_times, dtype=np.float64)
 
 			query_clean = cleanup_query(request_url)
-			return (query_clean + '\t' + str(cold_exec_time) + '\t' + str(result_size) + '\n')
-	
-def worker_pool(log_file):
+			return (query_clean + '\t' + str(warm_mean) + '\t' + str(cold_exec_time) + '\t' + str(result_size) + '\n')
+
+def main():
 	results = []
 	# with open(log_file) as f:
 	# 	#Spawn pool of workers to execute http queries
@@ -83,26 +91,23 @@ def worker_pool(log_file):
 	# 		print "Waiting for", remaining, "tasks to complete..."
 	# 		sys.stdout.flush()
 	# 		time.sleep(10)
-	with open(log_file) as in_:
+	count = 0
+	with open('database.log') as in_:
 		for l_ in in_:
 			res = run_log(l_)
 			if len(results) > 25000:
 				break
 			if res is not None:
-				print ".",
+				count +=1
+				print ".%d." % (count)
 				sys.stdout.flush()
 				results.append(res)
 
-	with open(log_file + '-out', 'a') as out:
+	with open(log_file + '-ran', 'a') as out:
 		for entry in results:
 		# for entry in results.get():
 			if entry is not None:
 				out.write(str(entry))
-
-
-def main():
-	worker_pool('random200k.log')
-
 
 if __name__ == '__main__':
 	main()
