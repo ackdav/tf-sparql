f2dd5546c3d5f8129c8f078067c94a452c28d57f
David <ack.dav@gmail.com>
2017-04-20 18:44:08 +0200

    rnn cleanup

--- graph_vec_nn/rnn.py
+++ graph_vec_nn/rnn.py
@@ -16,17 +16,12 @@ LOGDIR = 'logs/rnn/'
 
 hm_epochs = 100
 batch_size = 16
-# n_input = 52#66
 state_size = 24#66
 num_timesteps = 1#4 #How many steps to look back
 num_classes = 1
 num_layers = 3
 
 init_state = tf.placeholder(tf.float32, [num_layers, 2, batch_size, state_size])
-# init_state = [None for _ in range(num_layers)]
-# lens = tf.placeholder(tf.int32, [None])
-# leng = np.random.uniform(85, 125, batch_size).astype('int32')
-
 x = tf.placeholder(shape=[None, num_timesteps, state_size], dtype=tf.float32, name="x")
 y = tf.placeholder(shape=[None, num_classes], dtype=tf.float32, name="y")
 
@@ -51,18 +46,7 @@ def recurrent_neural_network(x, args, name="rnn-net"):
 	with tf.name_scope(name):
 		layer = {'weights':tf.Variable(tf.random_normal([state_size,num_classes])),
 						'biases':tf.Variable(tf.random_normal([num_classes]))}
-
-		if args.model == 'rnn':
-			cell_fn = rnn.BasicRNNCell
-		elif args.model == 'gru':
-			cell_fn = rnn.GRUCell
-		elif args.model == 'lstm':
-			cell_fn = rnn.BasicLSTMCell
-		elif args.model == 'nas': #only available from TF version 1.1
-			cell_fn = rnn.NASCell
-		else:
-			raise Exception("model type not supported: {}".format(args.model))
-
+						
 		#This unpacks the current state placeholer and assigns it
 		l = tf.unstack(init_state, axis=0)
 		rnn_tuple_state = tuple( [tf.contrib.rnn.LSTMStateTuple(l[idx][0], l[idx][1]) for idx in range(num_layers)])
@@ -72,15 +56,7 @@ def recurrent_neural_network(x, args, name="rnn-net"):
 		      state_size, reuse=tf.get_variable_scope().reuse)
 			return tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=0.8)
 
-		# with tf.variable_scope('layer'):
-		# 	cell = tf.contrib.rnn.LSTMCell(state_size, state_is_tuple=True)
-		# 	# cell = tf.contrib.rnn.NASCell(state_size)
-		# 	# cell = PhasedLSTMCell(state_size, use_peepholes=True, state_is_tuple=True)
-		# 	cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=0.8)
-			# tuple_state = list(rnn_tuple_state)
-			# outputs, current_state = multiPLSTM([cell for _ in range(num_layers)], x, leng, state_size, tuple_state)
 		rnn_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(num_layers)], state_is_tuple = True)
-
 		outputs, current_state = tf.nn.dynamic_rnn(rnn_cells, x, initial_state=rnn_tuple_state, scope = "layer")
 
 		outputs = tf.transpose(outputs, [1, 0, 2])
@@ -88,33 +64,6 @@ def recurrent_neural_network(x, args, name="rnn-net"):
 		#outputs gives all states, but we only want last one
 		output = tf.matmul(last, layer['weights']) + layer['biases']
 		return (output, current_state)
-
-# def multiPLSTM(cells, inputs, lens, n_input, initial_states):
-#     """
-#     Function to build multilayer PLSTM
-#     :param cells:
-#     :param inputs:
-#     :param lens: 2D tensor, length of the sequences in the batch (for synamic rnn use)
-#     :param n_input: integer, number of features in the input (without time feature)
-#     :param initial_states: list of tuples of initial states
-#     :return: 3D tensor, output of the multilayer PLSTM
-#     """
-    
-#     assert (len(initial_states) == len(cells))
-#     print "xo"
-#     print inputs.shape
-#     times = tf.slice(inputs, [0, 0, n_input], [-1, -1, 1])
-#     new_x = tf.slice(inputs, [0, 0, 0], [-1, -1, n_input])
-
-#     for k, cell, initial_state in zip(range(len(cells)), cells, initial_states):
-#         new_x = tf.concat(axis=2, values=[new_x, times])
-#         with tf.variable_scope("{}".format(k)):
-#             outputs, initial_states[k] = tf.nn.dynamic_rnn(cell, new_x, dtype=tf.float32,
-#                                                        sequence_length=lens,
-#                                                        initial_state=initial_state)
-#             new_x = outputs
-
-#     return new_x, initial_states[k]
 
 def train_neural_network(x, benchmark_err, args, log_param="cold_similarity"):
 	global X_test
@@ -124,7 +73,6 @@ def train_neural_network(x, benchmark_err, args, log_param="cold_similarity"):
 	begin_time = time.time()
 	# cost = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y, prediction))))
 	with tf.name_scope("relativeMeanError"):
-		# perc_err = tf.reduce_mean(tf.divide(tf.abs(tf.subtract(y, prediction)), tf.reduce_mean(y)))
 		perc_err = tf.reduce_mean(tf.divide(tf.abs(tf.subtract(y, prediction)), benchmark_err))
 		tf.summary.scalar("relativeMeanError", perc_err)
 	with tf.name_scope("optimizer"):
@@ -142,10 +90,6 @@ def train_neural_network(x, benchmark_err, args, log_param="cold_similarity"):
 			total_batch = num_training_samples//batch_size
 
 			for i in range(total_batch-1):
-				start_idx = i * num_timesteps
-				end_idx = start_idx + batch_size
-				# batch_x = X_train[start_idx:end_idx]
-				# batch_y = Y_train[start_idx:end_idx]
 				batch_x = X_train[i*batch_size:(i+1)*batch_size]
 				batch_y = Y_train[i*batch_size:(i+1)*batch_size]
 				
@@ -168,13 +112,7 @@ def train_neural_network(x, benchmark_err, args, log_param="cold_similarity"):
 
 			# Display logs per epoch step
 			if epoch % 1 == 0:
-				# biggst = 0.
-				# idx = 0
-				# for id_, _ in enumerate(err):
-				# 	if _ > biggst:
-				# 		biggst = _
-				# 		idx = id_
-				# print (label_value[idx], estimate[idx])
+
 				print ("Epoch:", '%04d' % (epoch+1), "cost=", \
 					"{:.9f}".format(c))
 				print ("[*]----------------------------")
@@ -199,21 +137,7 @@ def train_neural_network(x, benchmark_err, args, log_param="cold_similarity"):
 				# print ("relative error with model: {0:.3f}".format(perc_err.eval({x: X_test, y: Y_test, init_state: test_state})))
 				print ("relative error with model: {0:.3f}".format(avg_cost))
 				print ("relative error without model: {0:.3f}".format(benchmark_err))
-
-		# X_test = X_test.reshape((-1, num_timesteps, state_size))
-		# print ("RMSE: {:.3f}".format(cost.eval({x: X_test.reshape((-1, num_timesteps, state_size)), y: Y_test})))
-		# print ("relative error with model: {:.3f}".format(perc_err.eval({x: X_test.reshape((-1, num_timesteps, state_size)), y: Y_test})), "without model: {:.3f}".format(no_modell_mean_error(Y_train, Y_test)))
 		print("Total Time: %3.2fs" % float(time.time() - begin_time))
-
-# def plot_result(loss_vec, avg_cost_vec):
-# 	# Plot loss (MSE) over time
-# 	plt.plot(loss_vec, 'k-', label='Train Loss')
-# 	plt.plot(avg_cost_vec, 'r--', label='Test Loss')
-# 	# plt.plot(test_loss, 'b--', label='root squared mean error')
-# 	plt.title('Loss (MSE) per Generation')
-# 	plt.xlabel('Generation')
-# 	plt.ylabel('Loss')
-# 	plt.show()
 
 def main():
 	warm = False
